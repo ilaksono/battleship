7debed83838d9e551a164b14cfe5807e2bf2a699
ilaksono <60628041+ilaksono@users.noreply.github.com>
2020-10-11 14:32:49 -0400

    initialize ai set board

---
+++ helpers/computer.js
@@ -0,0 +1,12 @@
+module.exports = () => {
+  const setBoard = () => {};
+
+
+// row 0-9, col 0-9
+  const setShip = () => {
+    let can = Math.floor(Math.random() * 100);
+    let orient = Math.floor(Math.random() * 2);
+  };
+
+  return {};
+};
