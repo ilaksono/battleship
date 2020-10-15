2f011f8d7fa22cdeb9d67cf6ef13c55600290ef4
ilaksono <60628041+ilaksono@users.noreply.github.com>
2020-10-15 16:50:11 -0400

    add extras to archive

--- archive/battleship.js
+++ archive/battleship.js
@@ -1,10 +1,10 @@
 const generateBoard = size => {
   let arr = []
-  for(let i = 0; i < size; i++) {
+  for (let i = 0; i < size; i++) {
     let temp = [];
-    for(let j = 0; j < size; j++)
+    for (let j = 0; j < size; j++)
       temp.push('0');
-    arr[i] = temp; 
+    arr[i] = temp;
   }
   return arr;
 };
@@ -17,9 +17,9 @@ const placeShipsHorizontal = (board, size, coord) => {
 };
 
 const placeShipsVertical = (board, size, coord) => {
-  for(let row = coord[0]; row < coord[0] + size; row++) {
+  for (let row = coord[0]; row < coord[0] + size; row++) {
     board[row][coord[1]] = '1';
-  } 
+  }
 };
 
 let board = generateBoard(10);
@@ -32,18 +32,18 @@ let coord2 = [5, 1];
 let test2 = placeShipsVertical(board, 3, coord2);
 console.log(board);
 
-let testShot = [5,1];
+let testShot = [5, 1];
 const takeShot = (board, coord) => {
   if (board[coord[0]][coord[1]] == '1') {
     let temp = hitShip(board, coord);
-  } 
+  }
 }
 
 const hitShip = (board, coord) => {
   board[coord[0]][coord[1]] = 'X';
   console.log('HIT');
 }
- 
+
 takeShot(board, testShot);
 console.log(board);
 
